31819abab3324eca5a4db4521eed737fae11c855
Adam Melicher <adam.melicher101@gmail.com>
2022-03-14 14:44:32 +0100

    remove header tabs

--- quasarApp/src/layouts/MainLayout.vue
+++ quasarApp/src/layouts/MainLayout.vue
@@ -14,12 +14,6 @@
 
         <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
       </q-toolbar>
-
-      <q-tabs align="left">
-        <q-route-tab to="/page1" label="Page One" />
-        <q-route-tab to="/page2" label="Page Two" />
-        <q-route-tab to="/page3" label="Page Three" />
-      </q-tabs>
     </q-header>
 
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left" elevated>
