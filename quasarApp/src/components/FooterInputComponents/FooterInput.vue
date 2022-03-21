bb6516a3f0d16415ed8f2723a30905e120e0b988
Adam Melicher <adam.melicher101@gmail.com>
2022-03-21 17:20:04 +0100

    make autocomplete on footer messages input

--- quasarApp/src/components/FooterInputComponents/FooterInput.vue
+++ quasarApp/src/components/FooterInputComponents/FooterInput.vue
@@ -1,25 +1,75 @@
+<!--
 <template>
   <q-input dark color="primary" class="q-px-md" borderless v-model="input" placeholder="Type a message or command">
     <template v-slot:append>
-      <q-btn round dense color="grey-5"  flat icon="send" />
+      <q-btn round dense color="grey-5" flat icon="send" />
+    </template>
+    <template v-slot:prepend>
+      <q-btn round dense color="grey-5" flat icon="code" />
     </template>
   </q-input>
 </template>
+-->
+<template>
+      <q-select
+        dark
+        class="q-px-md"
+        placeholder="Type a message or command"
+        borderless
+        v-model="input"
+        use-input
+        hide-selected
+        fill-input
+        hide-dropdown-icon
+        input-debounce="0"
+        :options="options"
+        @filter="filterFunction"
+      >
+        <template v-slot:append>
+          <q-btn round dense color="grey-5" flat icon="send" />
+        </template>
+        <template v-slot:prepend>
+          <q-btn round dense color="grey-5" flat icon="code" />
+        </template>
+        <template v-slot:no-option>
+          <q-item>
+            <q-item-section class="text-grey">
+              No results
+            </q-item-section>
+          </q-item>
+        </template>
+      </q-select>
+</template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { ref, defineComponent } from 'vue';
+
+const commandList = [
+        'command1', 'command2', 'command3'
+      ]
 
 export default defineComponent({
-  name: 'FooterInput',
-  props: {
-  },
-  data () {
+
+  data() {
     return {
-      input : '',
+      options: ref(commandList),
+      input: '',
     }
   },
 
+  methods:{
+    filterFunction (val: string, update: (callbackFn: () => void, afterFn?: () => void) => void, abort: () => void) {
+        if (val.charAt(0) != '/') {
+          abort()
+          return
+        }
 
-});
+        update(() => {
+          const needle = val.substring(1).toLowerCase()
+          this.options = commandList.filter(v => v.toLowerCase().indexOf(needle) > -1)
+        })
+      }
+  },
+})
 </script>
 
