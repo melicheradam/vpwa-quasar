6e08753d72581aa34b2011b82202c2ee9ae7f74f
Adam Melicher <adam.melicher101@gmail.com>
2022-03-23 21:15:37 +0100

    auth middleware

---
+++ quasarApp/src/boot/auth.ts
@@ -0,0 +1,16 @@
+import { boot } from 'quasar/wrappers'
+
+export default boot(({ app, router, store }) => {
+  // something to do
+  router.beforeEach((to, from, next) => {
+    /*
+    // tu sa vzdy spytaj API ci si autorizovany, to kukne do DB ci je platna session
+    if(to.fullPath !== '/login'){
+        next('/login')
+    }
+    */
+    next()
+  })
+  
+})
+
